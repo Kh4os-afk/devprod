099fcf2c6573fd5b9f47cf944e9986b9fbf61016
Lucas Tabosa Martins de Lima <lucasdapoint@hotmail.com>
2025-12-08 20:38:47 -0400

    Description no Meta

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',[\App\Http\Controllers\IndexController::class,'index']);
 Route::post('/email',[\App\Http\Controllers\IndexController::class,'post']);
+
+Route::get('/teste',\App\Livewire\Index::class);
